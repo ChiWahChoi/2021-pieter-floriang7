e0200d68b4531ae4b1c27918ad72e2b55a97cec1
Florian Goossens <florian.goossens@student.hogent.be>
2021-04-28 16:33:44 +0200

    added MainNavComponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { BeerListComponent } from './beer/beer-list/beer-list.component';
 import { BeerModule } from './beer/beer.module';
 import { MaterialModule} from './material/material.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 const appRoutes: Routes = [
   { path: 'beer/list', component: BeerListComponent },
@@ -18,12 +25,19 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     PageNotFoundComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
     BeerModule,
     MaterialModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
