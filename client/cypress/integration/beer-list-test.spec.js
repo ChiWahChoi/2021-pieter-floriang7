54d85411444fcb2b2e9a9fb1643cdc85b8488891
Florian Goossens <florian.goossens@student.hogent.be>
2021-05-18 18:09:15 +0200

    added some extra tests

--- client/cypress/integration/beer-list-test.spec.js
+++ client/cypress/integration/beer-list-test.spec.js
@@ -2,7 +2,30 @@ describe('Beer List tests', function () {
     beforeEach(function () {
         cy.login();
       });
-      it('delayed response brings state out of sync', () => {
+      it('mock beer get, should display beers in the list', function() {
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: '/api/beers',
+            status: 200,
+            response: 'fixture:beers.json' 
+        });
+        cy.visit('/beer/list');
+        cy.get('[data-cy=beerCard]').should('have.length', 3);
+    });
+    it('mock beer get on error, should show error message', function() { 
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: '/api/beers',
+            status: 400,
+            response : 'ERROR'
+        });
+        cy.visit('/');
+        cy.get('[data-cy=appError]').should('be.visible'); 
+    });
+
+      it('mock beer get filtering by name, should display the correct beer', () => {
         cy.server();
         cy.route({
           method: 'GET',
@@ -10,27 +33,11 @@ describe('Beer List tests', function () {
           status: 200,
           response: 'fixture:beers.json'
         });
-        cy.route({
-          delay: 2000,
-          method: 'GET',
-          url: '/api/beers/?name=mad%20jack%20mixer',
-          status: 200,
-          response: 'fixture:mad-jack-mixer.json'
-        }).as('getSPrecipes');
-        cy.route({
-          method: 'GET',
-          url: '/api/beers/?name=keystone%20ice',
-          status: 200,
-          response: 'fixture:keystone-ice.json'
-        }).as('getKEYSTONEICEbeer');
 
         cy.visit('/');
-        cy.get('[data-cy=filterInput]').type('mad jack mixer');
-        cy.wait(300);
-        cy.get('[data-cy=filterInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}keystone ice');
-        cy.wait(['@getKEYSTONEICEbeer', '@getKEYSTONEICEbeer']);
+        cy.get('[data-cy=filterInput]').type('keystone ice');
         cy.get('[data-cy=beerCard]').should('have.length', 1);
-        cy.get('[data-cy=beer-title]').should('contain', 'Keystone');
-
+        cy.get('[data-cy=beerTitle]').should('contain', 'Keystone');
       });
+
 });
--- client/cypress/integration/test.spec.js
+++ client/cypress/integration/test.spec.js
@@ -3,70 +3,44 @@ describe('First tests', function() {
         cy.login();
       });
     it('App runs', function() {
-        cy.visit('/');
+        cy.visit('/beer/list');
         cy.get('[data-cy=filterInput]').type('mad');
         cy.get('[data-cy=beerCard]').should('have.length', 13);
     });
-    it('mock beer get', function() {
-        cy.server();
-        cy.route({
-            method: 'GET',
-            url: '/api/beers',
-            status: 200,
-            response: 'fixtures:beers.json' 
-        });
-        cy.visit('/');
-        cy.get('[data-cy=beerCard]').should('have.length', 3);
-    });
-    it('on error should show error message', function() { 
-        cy.server();
-        cy.route({
-            method: 'GET',
-            url: '/api/beers',
-            status: 400,
-            response : 'ERROR'
-        });
-        cy.visit('/');
-        cy.get('[data-cy=appError]').should('be.visible'); 
-    });
 });
 
 
-//FORM TESTING
-describe('Testing add review form', function() {
+//ADD REVIEW
+describe('Tests add review form', function() {
     beforeEach(function () {
         cy.login();
       });
-    it('valid data should create a new review', function() {
-        cy.server();
-        cy.route({
-            method: 'GET',
-            url: '/api/beers/1',
-            status: 200,
-            response : 'fixture:mad-jack-mixer.json'
-        });
-
-        cy.visit('/beer/list/detail/2')
-        cy.get('[data-cy=ratingInput]')
-            .type('7')
-            .should('have.value', '7')
-        cy.get('[data-cy=descriptionInput]')
-            .type('this is a description of a review for testing purposes')
-            .should('have.value', 'this is a description of a review for testing purposes')
-
-        cy.get('[data-cy=addReviewButton]').click()
-    });
-    it('invalid data should display an error message', function() {
-        cy.visit('/beer/list/detail/2')
-        cy.get('[data-cy=ratingInput]')
-            .type('12')
-            .should('have.value', '12')
-        cy.get('[data-cy=descriptionInput]')
-            .type('this is a description of a review for testing purposes')
-            .should('have.value', 'this is a description of a review for testing purposes')
-
-        cy.get('[data-cy=addReviewButton]').click()
+    it('invalid rating should display an error message', function() {
+        cy.visit('/beer/list/detail/1');
+        cy.get('[data-cy=ratingInput]').type('12');
+        //cy.get('[data-cy=descriptionInput]').type('');
+        cy.get('[data-cy=addReviewButton]').click();
 
         cy.get('[data-cy=ratingError]').should('be.visible')
+    }); 
+    it('valid rating & description, should create a new review', function() {
+        cy.visit('/beer/list/detail/1');
+        cy.get('[data-cy=ratingInput]').type('8');
+        cy.get('[data-cy=descriptionInput]').type('this is a test description from cypress');
+        cy.get('[data-cy=addReviewButton]').click();
+
+        cy.contains('this is a test description from cypress').should('be.visible');
     });
+
 });
+
+
+//DELETE REVIEW
+/*describe('Test delete review', function() {
+    beforeEach(function () {
+        cy.login();
+      });
+    it('clicking delete button, should delete review', function() {
+        cy.visit('/beer/list/detail/1');
+    });
+});*/
